cda524651565a347b53cc24e220c633b09ef10ac
Kothuri Saiteja <kothurisaiteja1@gmail.com>
2026-08-13 00:23:28 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -39,8 +39,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        # Add your Vercel URL here after deployment
-        # "https://your-app.vercel.app",
+       "https://movie-recommender-byteja.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
